6104325f67123995c707ee2ee3fdf9cd86ec1249
Dayton Schuh <daytonschuh@gmail.com>
2021-11-06 15:31:54 -0700

    Changed Home banner, added typewriter effect, added more projects, contact section opens default mail application

--- js/custom/main.js
+++ js/custom/main.js
@@ -4,28 +4,28 @@
 
     /*==================================================================
     [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
+    $('.input100').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
                 $(this).addClass('has-val');
             }
             else {
                 $(this).removeClass('has-val');
             }
-        })    
+        })
     })
 
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
 
@@ -33,20 +33,20 @@
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -63,17 +63,89 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
+
 
     /*==================================================================
     [ Show / hide Form ]*/
-    
-    $('.contact100-btn-hide').on('click', function(){
+
+    $('.contact100-btn-hide').on('click', function () {
         $('.wrap-contact100').fadeOut(400);
     })
 
-    $('.contact100-btn-show').on('click', function(){
+    $('.contact100-btn-show').on('click', function () {
         $('.wrap-contact100').fadeIn(400);
     })
 
+    /*==================================================================
+    [ Submit Email ]*/
+
+    $('#submitEmailButton').on('click', function () {
+        $('#emailForm').attr('action',
+            'mailto:daytonschuh@gmail.com?subject=Everything%20is%20awful');
+    })
+
 })(jQuery);
+
+/*==================================================================
+[ Typo Write ]*/
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    var i = this.loopNum;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (this.loopNum < this.toRotate.length && this.txt.slice(-1) === '!') {
+        document.getElementById('helloWorldTypo').classList.add('flashblue');
+        setTimeout(function () {
+            document.getElementById('helloWorldTypo').classList.remove('flashblue');
+        }, 2000);
+        this.loopNum++;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt && this.loopNum < this.toRotate.length-1) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        if (that.loopNum < that.toRotate.length) {
+            that.tick();
+        }
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typowrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+};
